03257e6aa2974ae98d381428465db5677d4fb0dc
tobias-g1 <tobias.gooney@icloud.com>
2022-02-28 12:19:39 +0000

    Add description to page header

--- src/shared/components/page-header/page-header.tsx
+++ src/shared/components/page-header/page-header.tsx
@@ -1,11 +1,12 @@
-import './page-header.scss'
+import "./page-header.scss";
 
 function PageHeaderComponent(props: any) {
-    return (
-        <div className="page-header">
-        <h2> { props.header } </h2>
-     </div>
-    );
+  return (
+    <div className="page-header">
+      <h2> {props.header} </h2>
+      <p> {props.description} </p>
+    </div>
+  );
 }
 
 export default PageHeaderComponent;
