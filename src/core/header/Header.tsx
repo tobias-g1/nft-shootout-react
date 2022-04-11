16becca786352794a7ab0dc0fb1a97ece2f29704
tobias-g1 <tobias.gooney@icloud.com>
2022-04-11 21:44:54 +0100

    Menu responsive

--- src/core/header/Header.tsx
+++ src/core/header/Header.tsx
@@ -3,15 +3,21 @@ import './Header.scss';
 import logo from '../../assets/img/logo.png'
 import { Link } from 'react-router-dom';
 import AuthComponent from "../../shared/components/auth/auth";
+import { faTimes, faBars, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const { Header } = Layout;
 
 function HeaderComponent() {
+
+  ;let showMenu = false;
+
   return (
-    <Header>
+    <>
+        <Header>
       <div className="left-content">
       <img className="logo" src={logo} alt="Logo"></img>
-        <ul>
+        <ul className="wide">
           <Link to="play"><li>Play</li></Link>
           <Link to="store"><li>Store</li></Link>
           <Link to="open"><li>Open</li></Link>
@@ -21,8 +27,12 @@ function HeaderComponent() {
       </div>
       <div className="right-content">
         <AuthComponent />
+        <FontAwesomeIcon className="menu narrow" icon={faBars} />
       </div>
     </Header>
+    </>
+
+    
   );
 }
 
