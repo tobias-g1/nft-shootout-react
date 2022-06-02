f5e6dfcd31370b5600d86b7403887e5131417b18
tobias-g1 <tobias.gooney@icloud.com>
2022-06-02 15:08:11 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ConfigProvider } from 'antd';
-
-ConfigProvider.config({
-  theme: {
-    primaryColor: '#8CC641'
-  },
-});
 
 ReactDOM.hydrate(
   <React.StrictMode>
